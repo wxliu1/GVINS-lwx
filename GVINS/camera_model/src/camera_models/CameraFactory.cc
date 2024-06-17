77d063de9e0ed5f7da451459726eace483eba9dd
liuwenxue <andy.liu@zichuan-tech.com>
2024-06-17 18:15:37 +0800

    Add comments to GVINS in Chinese.

---
+++ GVINS/camera_model/src/camera_models/CameraFactory.cc
@@ -0,0 +1,192 @@
+#include "camodocal/camera_models/CameraFactory.h"
+
+#include <boost/algorithm/string.hpp>
+
+
+#include "camodocal/camera_models/CataCamera.h"
+#include "camodocal/camera_models/EquidistantCamera.h"
+#include "camodocal/camera_models/PinholeCamera.h"
+#include "camodocal/camera_models/ScaramuzzaCamera.h"
+
+#include "ceres/ceres.h"
+
+namespace camodocal
+{
+
+boost::shared_ptr<CameraFactory> CameraFactory::m_instance;
+
+CameraFactory::CameraFactory()
+{
+
+}
+
+boost::shared_ptr<CameraFactory>
+CameraFactory::instance(void)
+{
+    if (m_instance.get() == 0)
+    {
+        m_instance.reset(new CameraFactory);
+    }
+
+    return m_instance;
+}
+
+CameraPtr
+CameraFactory::generateCamera(Camera::ModelType modelType,
+                              const std::string& cameraName,
+                              cv::Size imageSize) const
+{
+    switch (modelType)
+    {
+    case Camera::KANNALA_BRANDT:
+    {
+        EquidistantCameraPtr camera(new EquidistantCamera);
+
+        EquidistantCamera::Parameters params = camera->getParameters();
+        params.cameraName() = cameraName;
+        params.imageWidth() = imageSize.width;
+        params.imageHeight() = imageSize.height;
+        camera->setParameters(params);
+        return camera;
+    }
+    case Camera::PINHOLE:
+    {
+        PinholeCameraPtr camera(new PinholeCamera);
+
+        PinholeCamera::Parameters params = camera->getParameters();
+        params.cameraName() = cameraName;
+        params.imageWidth() = imageSize.width;
+        params.imageHeight() = imageSize.height;
+        camera->setParameters(params);
+        return camera;
+    }
+    case Camera::SCARAMUZZA:
+    {
+        OCAMCameraPtr camera(new OCAMCamera);
+
+        OCAMCamera::Parameters params = camera->getParameters();
+        params.cameraName() = cameraName;
+        params.imageWidth() = imageSize.width;
+        params.imageHeight() = imageSize.height;
+        camera->setParameters(params);
+        return camera;
+    }
+    case Camera::MEI:
+    default:
+    {
+        CataCameraPtr camera(new CataCamera);
+
+        CataCamera::Parameters params = camera->getParameters();
+        params.cameraName() = cameraName;
+        params.imageWidth() = imageSize.width;
+        params.imageHeight() = imageSize.height;
+        camera->setParameters(params);
+        return camera;
+    }
+    }
+}
+
+CameraPtr
+CameraFactory::generateCameraFromYamlFile(const std::string& filename)
+{
+    /*
+     * 程序编译通过，运行时却报错Segmentation fault
+     * 问题似乎出现在noetic的cv_bridge与本机的OpenCV版本无法兼容导致出现错误
+     * 由于ubnutu20.04安装ros时自带opencv4.2
+     * 究其原因，是因为在安装ROS时，cv_bridge默认连接了4.2版本的opencv进行编译
+     * 而find_package(OpenCV REQUIRED)连接的是系统默认安装的OpenCV3，故而出现问题
+     * 解决办法要么是重新编译一个适配opencv3(即，跟find_package在系统中查找到的OpenCV版本一致的)的cv_bridge
+     * 要么就是find_package(OpenCV 4 REQUIRED), 然后解决编译报错的地方，加上一些头文件即可。
+     */
+    cv::FileStorage fs(filename, cv::FileStorage::READ);
+
+    /*
+     * 添加的调试代码不过没有验证
+    cv::FileStorage fs;
+    try{
+        fs.open(filename, cv::FileStorage::READ);
+    }catch(cv::Exception ex)
+    {
+        return CameraPtr();
+    }*/
+
+    if (!fs.isOpened())
+    {
+        return CameraPtr();
+    }
+
+    Camera::ModelType modelType = Camera::MEI;
+    if (!fs["model_type"].isNone())
+    {
+        std::string sModelType;
+        fs["model_type"] >> sModelType;
+
+        if (boost::iequals(sModelType, "kannala_brandt"))
+        {
+            modelType = Camera::KANNALA_BRANDT;
+        }
+        else if (boost::iequals(sModelType, "mei"))
+        {
+            modelType = Camera::MEI;
+        }
+        else if (boost::iequals(sModelType, "scaramuzza"))
+        {
+            modelType = Camera::SCARAMUZZA;
+        }
+        else if (boost::iequals(sModelType, "pinhole"))
+        {
+            modelType = Camera::PINHOLE;
+        }
+        else
+        {
+            std::cerr << "# ERROR: Unknown camera model: " << sModelType << std::endl;
+            return CameraPtr();
+        }
+    }
+
+    switch (modelType)
+    {
+    case Camera::KANNALA_BRANDT:
+    {
+        EquidistantCameraPtr camera(new EquidistantCamera);
+
+        EquidistantCamera::Parameters params = camera->getParameters();
+        params.readFromYamlFile(filename);
+        camera->setParameters(params);
+        return camera;
+    }
+    case Camera::PINHOLE:
+    {
+        PinholeCameraPtr camera(new PinholeCamera);
+
+        PinholeCamera::Parameters params = camera->getParameters();
+        params.readFromYamlFile(filename);
+        camera->setParameters(params);
+        return camera;
+    }
+    case Camera::SCARAMUZZA:
+    {
+        OCAMCameraPtr camera(new OCAMCamera);
+
+        OCAMCamera::Parameters params = camera->getParameters();
+        params.readFromYamlFile(filename);
+        camera->setParameters(params);
+        return camera;
+    }
+    case Camera::MEI:
+    default:
+    {
+        CataCameraPtr camera(new CataCamera);
+
+        CataCamera::Parameters params = camera->getParameters();
+        params.readFromYamlFile(filename);
+        camera->setParameters(params);
+        return camera;
+    }
+    }
+
+    return CameraPtr();
+}
+
+}
+
